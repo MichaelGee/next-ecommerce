3bdc5dffb323cd7704e29291010e9b26ffc8e62a
michael.gates <michaelgatesjr@gmail.com>
2021-09-02 17:17:57 +0100

    feat: modal context and navbar login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { Navbar } from 'components/organisms/Navbar/Navbar'
 import Head from 'next/head'
 import { FC } from 'react'
 
@@ -10,7 +9,6 @@ const Home: FC = () => {
         <title>Grandior</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar/>
     </>
   )
 }
