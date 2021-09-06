0e3ee46c66c9bc2841bda3a1f9e1edbbf8eb7330
MichaelGee <michaelgatesjr@gmail.com>
2021-09-06 16:55:40 +0100

    fix: deleted document file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
 import { StyleReset, ThemeProvider } from 'atomize';
 import { Provider as StyletronProvider } from 'styletron-react'
-import { styletron } from 'styletron'
+import { Styletron } from '../styletron.js'
 import  mainTheme  from 'theme'
 import { ModalProvider } from 'contexts/ModalContext';
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StyletronProvider value={styletron}>
+    <StyletronProvider value={Styletron}>
       <ModalProvider>
       <ThemeProvider theme={mainTheme}>
         <StyleReset />
--- styletron.js
+++ styletron.js
@@ -3,7 +3,7 @@ import { Client, Server } from 'styletron-engine-atomic'
 const getHydrateClass = () =>
   document.getElementsByClassName('_styletron_hydrate_')
 
-export const styletron =
+export const Styletron =
   typeof window === 'undefined'
     ? new Server()
     : new Client({
